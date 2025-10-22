fb74a6e6ba8a7f17bdcadfdf75b4bdf52059a2b3
Chris Lasell <chrisl@pixar.com>
2025-10-22 15:47:20 -0700

    remove unneeded comment lines

--- gems/admin/lib/xolo/admin/highline_terminal.rb
+++ gems/admin/lib/xolo/admin/highline_terminal.rb
@@ -1,30 +1,22 @@
-# Copyright 2025 Pixar
-#
-#    Licensed under the terms set forth in the LICENSE.txt file available at
-#    at the root of this project.
-#
-#
-
 # frozen_string_literal: true
 
-# MonkeyPatch HighLine::Terminal#readline_read so that Readline
-# lines can be case-insensitive, and have a prompt.
-#
-# To use a prompt, put it in the environtment variable 'XADM_HIGHLINE_READLINE_PROMPT'
-#
-# To make the readline completion case-insensitive, set the environment
-# variable XADM_HIGHLINE_READLINE_CASE_INSENSITIVE to anything.
-#
-# This really only modifies the Regexp used for the completion_proc to make it
-# case insensitive if desired (adding an 'i' to the end)
-# and sets the prompt when calling Readline.readline
-#
-# TODO: Do this 'smartly' as with the monkeypatches in pixar-ruby-extensions
-#
 class HighLine
 
   class Terminal
 
+    # MonkeyPatch HighLine::Terminal#readline_read so that Readline
+    # lines can be case-insensitive, and have a prompt.
+    #
+    # To use a prompt, put it in the environtment variable 'XADM_HIGHLINE_READLINE_PROMPT'
+    #
+    # To make the readline completion case-insensitive, set the environment
+    # variable XADM_HIGHLINE_READLINE_CASE_INSENSITIVE to anything.
+    #
+    # This really only modifies the Regexp used for the completion_proc to make it
+    # case insensitive if desired (adding an 'i' to the end)
+    # and sets the prompt when calling Readline.readline
+    #################################
+    #
     # Use readline to read one line
     # @param question [HighLine::Question] question from where to get
     #   autocomplete candidate strings
@@ -51,31 +43,6 @@ class HighLine
       raw_answer
     end
 
-    # Get one line from terminal using default #gets method.
-    ##############################
-    # def get_line_default(highline)
-    #   raise EOFError, 'The input stream is exhausted.' if highline.track_eof? && highline.input.eof?
-
-    #   highline.output.print "#{ENV['XADM_HIGHLINE_LINE_PROMPT']}" if ENV['XADM_HIGHLINE_LINE_PROMPT']
-
-    #   highline.input.gets
-    # end
-
-  end # terminal
-
-  # # Deals with the task of "asking" a question
-  # class QuestionAsker
-
-  #   alias ask_once_real ask_once
-
-  #   # Gets just one answer, as opposed to #gather_answers
-  #   #
-  #   # @return [String] answer
-  #   def ask_once
-  #     @highline.output.print "#{ENV['XADM_HIGHLINE_LINE_PROMPT']}" if ENV['XADM_HIGHLINE_LINE_PROMPT']
-  #     ask_once_real
-  #   end
-
-  # end # question asker
+  end # class Terminal
 
 end
